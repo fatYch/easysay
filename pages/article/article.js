eea12b8b7f85f1c9fe6c225313145d682a8b9901
ych <897141839@qq.com>
2020-03-25 01:07:49 +0800

    增加详情，列表功能

--- pages/article/article.js
+++ pages/article/article.js
@@ -1,1 +1,83 @@
-Page({})
+Page({
+
+  data: {
+    article: "",
+    loveUrl: "../images/flower-no.png",
+    isLove: true,
+    complainDialog: false,
+    hasComplain: false,
+    complainUrl: "../images/complain.png",
+    buttons: [{
+      text: '取消',
+      name: 'cancel',
+      bindtap: 'complainCancel'
+    }, {
+      text: '确定',
+      name: 'confirm',
+      bindtap: 'complainConfirm'
+    }]
+  },
+
+  onLoad: function(e) {
+    var articleId = e.id;
+    var that = this;
+    //请求文章详情
+    wx.request({
+      url: 'http://47.107.71.7:8088/article/detail/' + articleId,
+      success(res) {
+        console.info(res.data);
+        that.setData({
+          article: res.data.data
+        })
+      }
+    })
+  },
+
+  //喜欢
+  loveIt: function(e) {
+    var url;
+    if (this.data.isLove) {
+      url = "../images/flower.png";
+    } else {
+      url = "../images/flower-no.png"
+    }
+    this.setData({
+      loveUrl: url,
+      isLove: !this.data.isLove
+    })
+  },
+
+  //发起举报
+  complain: function(e) {
+    if (this.data.hasComplain) {
+      wx.showToast({
+        title: '你已经举报过了，请手下留情！',
+        icon: 'none',
+        duration: 2000
+      })
+    } else {
+        this.setData({
+          complainDialog: true
+        })
+    }
+  },
+
+  //举报选择
+  complainSelect: function(e){
+    var result = false;
+    if ('confirm' == e.detail.item.name){
+      //处理业务
+      console.info("执行业务");
+      result = true;
+    }
+    this.setData({
+      complainDialog: false,
+      hasComplain: result
+    })
+  }
+
+
+
+
+
+})
